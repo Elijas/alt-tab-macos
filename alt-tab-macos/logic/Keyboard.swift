Elijas/alt-tab-macos

commit ac2cf3a1f44c7f0c2670d79e6054d35e658a5d0e

refactor: keyboard event listening should be read-only

alt-tab-macos/logic/Keyboard.swift
@@ -27,8 +27,8 @@ func listenToGlobalKeyboardEvents(_ delegate: Application) {
         eventTap = CGEvent.tapCreate(
                 tap: .cgSessionEventTap,
                 place: .headInsertEventTap,
-                options: .defaultTap,
-                eventsOfInterest: CGEventMask(eventMask),
+                options: .listenOnly,
+                eventsOfInterest: eventMask,
                 callback: { (_, _, event, delegate_) -> Unmanaged<CGEvent>? in
                     let d = Unmanaged<Application>.fromOpaque(delegate_!).takeUnretainedValue()
                     return keyboardHandler(event, d)

commit e17a919e6f67f78545a12191c7386a6639addcc8

fix: app was no longer absorbing its shortcut key events properly
Regression was introduced by ac2cf3a1f4

alt-tab-macos/logic/Keyboard.swift
@@ -15,7 +15,7 @@ func listenToGlobalKeyboardEvents(_ delegate: Application) {
         eventTap = CGEvent.tapCreate(
                 tap: .cgSessionEventTap,
                 place: .headInsertEventTap,
-                options: .listenOnly,
+                options: .defaultTap,
                 eventsOfInterest: eventMask,
                 callback: { (_, _, event, delegate_) -> Unmanaged<CGEvent>? in
                     let d = Unmanaged<Application>.fromOpaque(delegate_!).takeUnretainedValue()