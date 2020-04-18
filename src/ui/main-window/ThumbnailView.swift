Elijas/alt-tab-macos

commit 9f906090ddc31d8e2d6b19db8417b22f4bc60ae7

fix: avoid text flickering on main ui (closes #197)

src/ui/main-window/ThumbnailView.swift
@@ -42,6 +42,8 @@ class ThumbnailView: NSStackView {
             label.string = element.title
             // workaround: setting string on NSTextView change the font (most likely a Cocoa bug)
             label.font = Preferences.font
+            // force a display to avoid flickering; see https://github.com/lwouis/alt-tab-macos/issues/197
+            label.display()
         }
         assignIfDifferent(&hiddenIcon.isHidden, !window_!.isHidden)
         assignIfDifferent(&minimizedIcon.isHidden, !window_!.isMinimized)

commit 4e49b06e68a336df5046fe801b5db6f43aa3bcb0

fix: thumbnails layout was wrong sometimes
this is a regression introduced by 9f90609

src/ui/main-window/ThumbnailView.swift
@@ -24,7 +24,6 @@ class ThumbnailView: NSStackView {
 
     func updateRecycledCellWithNewContent(_ element: Window, _ mouseDownCallback: @escaping MouseDownCallback, _ mouseMovedCallback: @escaping MouseMovedCallback, _ newHeight: CGFloat, _ screen: NSScreen) {
         window_ = element
-//        element.itemView = self
         if thumbnail.image != element.thumbnail {
             thumbnail.image = element.thumbnail
             let (thumbnailWidth, thumbnailHeight) = ThumbnailView.thumbnailSize(element.thumbnail, screen)
@@ -38,12 +37,11 @@ class ThumbnailView: NSStackView {
             appIcon.image?.size = appIconSize
             appIcon.frame.size = appIconSize
         }
-        if label.string != element.title {
+        let labelChanged = label.string != element.title
+        if labelChanged {
             label.string = element.title
             // workaround: setting string on NSTextView change the font (most likely a Cocoa bug)
             label.font = Preferences.font
-            // force a display to avoid flickering; see https://github.com/lwouis/alt-tab-macos/issues/197
-            label.display()
         }
         assignIfDifferent(&hiddenIcon.isHidden, !window_!.isHidden)
         assignIfDifferent(&minimizedIcon.isHidden, !window_!.isMinimized)
@@ -67,6 +65,11 @@ class ThumbnailView: NSStackView {
         } else if trackingAreas.count > 0 && trackingAreas[0].rect != bounds {
             removeTrackingArea(trackingAreas[0])
             addTrackingArea(NSTrackingArea(rect: bounds, options: [.mouseMoved, .activeAlways], owner: self, userInfo: nil))
+        }
+        // force a display to avoid flickering; see https://github.com/lwouis/alt-tab-macos/issues/197
+        // quirk: display() should be called last as it resets thumbnail.frame.size somehow
+        if labelChanged {
+            label.display()
         }
     }
 